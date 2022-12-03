2475a1167f0115f13b609be7b226db70acb04a6c
tk42 <nsplat@gmail.com>
2022-12-03 18:22:47 +0900

    Fixed entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 if [ -n $ENDPOINT ] ; then
   if [ -n $ACCESS_TOKEN ] ; then
-    get-graphql-schema -h 'Authorization=Bearer '$ACCESS_TOKEN $ENDPOINT > schema.graphql
+    get-graphql-schema -h 'Authorization=Bearer '$ACCESS_TOKEN $ENDPOINT > ./dist/schema.graphql
   else
-    get-graphql-schema $ENDPOINT > schema.graphql
+    get-graphql-schema $ENDPOINT > ./dist/schema.graphql
   fi
 else
   cd /home/build
